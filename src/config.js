c2c39d2b67587ebc0cc626e8a211abf82c48f744
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-21 20:14:16 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,55 @@
 export const API_KEY = '49403bed-8521d5cd-130b226a-b667787e';
-export const API_URL_LIST = 'https://fortniteapi.io/v2/items/list?lang=ru';
+export const API_URL_LIST = '{
+    "result": true,
+    "items": [
+        {
+            "id": "CID_267_Athena_Commando_M_RobotRed",
+            "type": {
+                "id": "outfit",
+                "name": "Экипировка"
+            },
+            "name": "Б.О.Т.",
+            "description": "Боевой опасный терминатор.",
+            "rarity": {
+                "id": "Legendary",
+                "name": "ЛЕГЕНДАРНЫЙ"
+            },
+            "series": null,
+            "price": 0,
+            "added": {
+                "date": "2018-09-27",
+                "version": "6.0"
+            },
+            "builtInEmote": null,
+            "copyrightedAudio": false,
+            "upcoming": false,
+            "reactive": false,
+            "releaseDate": null,
+            "lastAppearance": null,
+            "interest": 0.27,
+            "images": {
+                "icon": "https://media.fortniteapi.io/images/552cd39-3307b68-2aef1e3-414c094/transparent.png",
+                "featured": null,
+                "background": "https://media.fortniteapi.io/images/cosmetics/552cd39-3307b68-2aef1e3-414c094/v2/background.png",
+                "full_background": "https://media.fortniteapi.io/images/cosmetics/552cd39-3307b68-2aef1e3-414c094/v2/info.ru.png"
+            },
+            "video": null,
+            "audio": null,
+            "gameplayTags": [
+                "Cosmetics.Source.Season6.BattlePass.Paid",
+                "Cosmetics.Set.RobotRed",
+                "Cosmetics.Filter.Season.6"
+            ],
+            "apiTags": [],
+            "battlepass": null,
+            "set": {
+                "id": "RobotRed",
+                "name": "БОТ",
+                "partOf": "Входит в набор «БОТ»."
+            }
+        },
+        ..........
+    ]
+}';
 export const API_URL_ITEM = 'https://fortniteapi.io/v2/items/get?lang=ru';
 
